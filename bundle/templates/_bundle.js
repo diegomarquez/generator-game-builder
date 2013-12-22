1d37de9685615de70653808c8fe64d12acbf96f7
Diego <marquez.diego.e@gmail.com>
2013-12-22 08:32:05 +0000

    Add some comments to the bundle template to make more explicit all the things available to them

--- bundle/templates/_bundle.js
+++ bundle/templates/_bundle.js
@@ -1,6 +1,11 @@
 define(function(require) {	
 	//Insert require calls to other modules this bundle will use here
 
+	// This properties are available to a bundle
+	// this.gameObjectPool. A reference to the Game Objects Pool	
+	// this.componentPool. A reference to the Components Pool
+	// this.canvas. A reference to the canvas.
+
 	var <% print( _(name).classify() ) %> = require('bundle').extend({
 		create: function(args) {
 			//Add things to the gameObjectPool and componentPool objects here
