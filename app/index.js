0e00971d5edd7445005403e92ff13cbdf9d92217
Diego <marquez.diego.e@gmail.com>
2014-04-08 00:45:50 +0100

    Add font loading files

--- app/index.js
+++ app/index.js
@@ -165,6 +165,7 @@ GameBuilderGenerator.prototype.copyFiles = function projectfiles() {
   this.copy('create-config.js', 'tasks/create-config.js');
   this.copy('local-assets.js', 'tasks/local-assets.js');
   this.copy('Gruntfile.js', 'Gruntfile.js');
+  this.copy('font-data.js', 'font-data.js');
   this.copy('remote-assets.json', 'remote-assets.json');
 
 };
